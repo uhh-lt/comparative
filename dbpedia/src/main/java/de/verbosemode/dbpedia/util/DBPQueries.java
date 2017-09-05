922ec540a257944d4502bc88045d11253505e75b
Mirco <ablx@posteo.de>
2017-09-05 15:14:28 +0200

    feat($dbpedia): First PoC
    SparQL Queries to get the needed data
    
    #2

--- dbpedia/src/main/java/de/verbosemode/dbpedia/util/DBPQueries.java
+++ dbpedia/src/main/java/de/verbosemode/dbpedia/util/DBPQueries.java
@@ -3,21 +3,26 @@ package de.verbosemode.dbpedia.util;
 import lombok.SneakyThrows;
 import org.apache.jena.ext.com.google.common.collect.Lists;
 import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
 
 public class DBPQueryExec {
 
-    @SneakyThrows
-    public static QueryEngineHTTP exec(QueryString queryString) {
-        return exec(queryString.toQuery());
-    }
 
-    @SneakyThrows
-    public static QueryEngineHTTP exec(Query query) {
+    // TODO: use a local dbpedia copy
+    private static QueryEngineHTTP buildQueryEngine(Query query) {
         QueryEngineHTTP qexec = (QueryEngineHTTP) QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
         qexec.addParam("timeout", "10000");
         qexec.setDefaultGraphURIs(Lists.newArrayList("http://dbpedia.org"));
         return qexec;
+    }
+
+    public static ResultSet select(Query query) {
+        return buildQueryEngine(query).execSelect();
+    }
+
+    public static String getLabel(Resource resource) {
+        return  "";
     }
 
 
