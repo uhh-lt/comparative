359bffb1f7be8e119d702e5de8ea1cf713d65eb7
Mirco <ablx@posteo.de>
2017-09-05 13:46:00 +0200

    Prefixes

--- dbpedia/src/main/java/de/verbosemode/dbpedia/util/QueryString.java
+++ dbpedia/src/main/java/de/verbosemode/dbpedia/util/QueryString.java
@@ -1,26 +1,30 @@
 package de.verbosemode.dbpedia.util;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Singular;
+import lombok.*;
 import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryFactory;
 
+import java.util.AbstractMap;
+import java.util.List;
 import java.util.Map;
 
-@Log
+
 @Builder
 @AllArgsConstructor
+@Log
 public class QueryString {
 
+    public static Prefix RDF = Prefix.of().prefix("rdf").url("http://www.w3.org/1999/02/22-rdf-syntax-ns#").build();
+    public static Prefix OWL = Prefix.of().prefix("owl").url("http://www.w3.org/2002/07/owl#").build();
+    public static Prefix DBO = Prefix.of().prefix("dbo").url("http://dbpedia.org/ontology/").build();
+public static Prefix RDFS = Prefix.of().prefix("rdfs").url("http://www.w3.org/2000/01/rdf-schema#").build();
     // namespaces: http://eo.dbpedia.org/sparql?nsdecl
     @Singular
-    private Map<String, String> prefixes;
+    private List<Prefix> prefixes;
     private boolean distinct;
     private int limit;
     private String query;
@@ -28,7 +32,7 @@ public class QueryString {
 
     public Query toQuery() {
         StringBuffer buffer = new StringBuffer();
-        prefixes.entrySet().forEach(e -> buffer.append("PREFIX ").append(e.getKey()).append(": <").append(e.getValue()).append(">\n"));
+        prefixes.forEach(e -> buffer.append("PREFIX ").append(e.getPrefix()).append(": <").append(e.getUrl()).append(">\n"));
         buffer.append("SELECT ");
         if (distinct) {
             buffer.append("DISTINCT ");
@@ -37,9 +41,16 @@ public class QueryString {
         if (limit > 0) {
             buffer.append(" LIMIT ").append(limit);
         }
-
+        log.info(buffer.toString());
         return QueryFactory.create(buffer.toString());
 
+    }
+
+    @Data
+    @Builder(builderMethodName = "of")
+    public static class Prefix {
+        public String prefix;
+        public String url;
     }
 
 }
