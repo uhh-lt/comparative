98c6cc202b3a896957a658343a87f47188f1ee94
Mirco <ablx@posteo.de>
2017-11-03 17:43:36 +0100

    Data for crowd sourcing experiments

--- 3. Crowdsourcing/preprocess.py
+++ 3. Crowdsourcing/preprocess.py
@@ -1,5 +1,6 @@
 import json
 import string
+
 from random import shuffle
 
 
@@ -14,7 +15,7 @@ def read(file):
 
 def length_filter(sentence):
     """sentece must be at least 15 chars long"""
-    return len(sentence) >= 15 and len(sentence) <= 100
+    return len(sentence) >= 15 and len(sentence) <= 200
 
 def appears_once(sentence, words):
     """each object should appear exactly once"""
@@ -27,20 +28,37 @@ def not_contains(sentence,words):
     count = 0
     for word in words:
         count += sentence.count(word)
-    return count == 0   
+    return count == 0
 
 
-def symbols(sentence, max=5):
+def symbols(sentence, max=7):
     """not to much punctuation"""
     count = 0
     for punct in string.punctuation:
         count += sentence.count(punct)
     return count <= max
 
+def replace_objects(sentence, objects):
+    sentence_lower = sentence.lower()
+    a_start = sentence_lower.index(objects[0].lower())
+    b_start = sentence_lower.index(objects[1].lower())
+    if a_start < b_start:
+        first = sentence[:a_start] + 'OBJECT_A'
+        middle = sentence[a_start+len(objects[0]):b_start]
+        end = 'OBJECT_B' + sentence[b_start+len(objects[1]):]
+        return {'original': sentence,  'replaced' :first+middle+end }
+    else:
+        first = sentence[:b_start] + 'OBJECT_A'
+        middle = sentence[b_start+len(objects[1]):a_start]
+        end = 'OBJECT_B' + sentence[a_start+len(objects[0]):]
+        return {'original': sentence, 'replaced': first + middle + end}
+
+
+
 if __name__ == '__main__':
 
 
-    objs = [['ruby', 'python'], ['android','iphone'],['cat','dog'], ['car','bicycle'], ['summer', 'winter'], 
+    objs = [['ruby', 'python'], ['android','iphone'],['cat','dog'], ['car','bicycle'], ['summer', 'winter'],
     ['bmw','mercedes'], ['wine','beer'], ['usa', 'europe'], ['football','baseball'],['chicken', 'beef']]
 
     sentences = []
@@ -51,9 +69,14 @@ if __name__ == '__main__':
         }
         for file, objects in files.items():
             lines = read('data/{}.json'.format(file))
-            filtered = [line+'\n' for line in lines if length_filter(line) and appears_once(line, objects[0]) and symbols(line) and not_contains(line, objects[1])]
+            filtered = [{'source': file, 'text':replace_objects(line,objects[0:1][0])} for line in lines if length_filter(line) and appears_once(line, objects[0]) and symbols(line) and not_contains(line, objects[1])]
             shuffle(filtered)
             sentences += filtered[:objects[2]]
 
-    with open('result.txt', 'w') as file:
-        file.writelines(sentences)
+
+    with open('result-replaced-objects.json', 'w') as file:
+        json.dump(sentences, file)
+
+    with open('result-replaced-sentences.txt', 'w') as file1:
+        for obj in sentences:
+            file1.write(obj['text']['replaced']+'\n')
